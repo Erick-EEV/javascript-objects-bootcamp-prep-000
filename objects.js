9c817797330ec959a4cc917dd85d7e6207ee896d
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 03:43:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {Object};
 
 function updatePlaylist(){
   var playlist = new Object({artistName: songTitle})
-  return Object
+  return updatePlaylist
 }
 
 function removeFromPlaylist(){
