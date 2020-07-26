9001aa740b7d563d8ef6c716b2948c9dc493955e
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 02:31:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,11 @@ var meals = {breakfast: "oatmeal"};
 var meals = new Object({breakfast: `oatmeal`});
 var meals = {[firstmeal]: `oatmeal` }
 meal;
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
+const recipe = {
+}
