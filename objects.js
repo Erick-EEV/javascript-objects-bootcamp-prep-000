cf90b9933ae128810843291c2d14fea7e0c724f0
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 02:35:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,13 +2,14 @@ var meals = {};
 var meals = new Object();
 var meals = {breakfast: "oatmeal"};
 var meals = new Object({breakfast: `oatmeal`});
-var meals = {[firstmeal]: `oatmeal` }
+var meals = {[firstmeal]: `oatmeal` };
 meal;
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
-  obj[key] = value
-  return obj
+  obj[key] = value;
+  return obj;
 }
 
-const recipe = {
-}
+const recipe = { eggs: 3};
+
+destructivelyUpdateObjectWithKeyAndValue(recipe, `flour`, `3 cups`)
