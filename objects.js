64a18b360391b7be2fd11575ef63635cfcdf12ba
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 02:47:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -18,3 +18,7 @@ Object.assign({}, {foo: `bar` })
 Object.assign({eggs: 3}, {flour: `1 cup` })
 
 Object.assign({eggs: 3}, { chocolate: `1 cup`, flour: `2 cups`}, { flour: `1/2 cup`})
+
+function updateObjectWithKeyAndValue(obj, key, value){
+  return Objet.assign({}, obj, {[key]: value })
+}
